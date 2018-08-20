842305fa735dae60c1754ffa7c2cfdeb76f8a1a3
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-08-20 17:57:17 -0300

    menu geral

---
+++ perfil/includes/menu_geral.php
@@ -0,0 +1,12 @@
+<div class="row">
+	<div class="form-group">
+		<div class="col-md-offset-2 col-md-8">
+			<strong>
+			| <a href="?secao=perfil">Início</a>
+			| <a href="?perfil=perfil">Carregar Módulos</a>
+			| <a href="" target="_blank">Ajuda</a>
+			| <a href="../include/logoff.php">Sair</a> |</strong><br/>
+		</div>
+	</div>
+</div>
+<br/>
