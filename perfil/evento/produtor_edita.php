35d50ca7200aa0f06bd761fc83d1e8d7a935c023
lucasdauto <31465480+lucasdauto@users.noreply.github.com>
2018-10-22 12:52:10 -0300

    Cinema

--- perfil/evento/produtor_edita.php
+++ perfil/evento/produtor_edita.php
@@ -1,36 +1,58 @@
 <?php
-$con = bancoMysqli();
-include "includes/menu_interno.php";
+    $con = bancoMysqli();
+    include "includes/menu_interno.php";
 
-//if(isset($_POST['idAtracao'])){
-//    $idAtracao = $_POST['idAtracao'];
-//}
+    //if(isset($_POST['idAtracao'])){
+    //    $idAtracao = $_POST['idAtracao'];
+    //}
 
+    if (isset($_POST['cadastra']) || isset($_POST['edita'])){
+        $idAtracoes = $_POST['idAtracoes'];
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $telefone1 = $_POST['telefone1'];
+        $telefone2 = $_POST['telefone2'];
+        $observacao = $_POST['observacao'];
+    }
+    if (isset($_POST['cadastra'])){
+        $sqlInsert = "INSERT INTO `produtores`
+                      (nome, email, telefone1, telefone2, observacao)
+                      VALUES ('$nome','$email','$telefone1','$telefone2','$observacao')";
 
-if (isset($_POST['cadastra']) || isset($_POST['edita'])){
-    $idAtracoes = $_POST['idAtracao'];
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $telefone1 = $_POST['telefone1'];
-    $telefone2 = $_POST['telefone2'];
-    $observacao = $_POST['observacao'];
-}
-if (isset($_POST['cadastra'])){
-    $sqlInsert = "INSERT INTO `produtores`
-                  (nome, email, telefone1, telefone2, observacao)
-                  VALUES ('$nome','$email','$telefone1','$telefone2','$observacao')";
-
-    if (mysqli_query($con,$sqlInsert)){
-        $idProdutor = recuperaUltimo("produtores");
-        $sqlUpdate = "UPDATE `atracoes`
-                      SET produtor_id = '$idProdutor' 
-                      WHERE id ='$idAtracoes'";
-        if(mysqli_query($con,$sqlUpdate)){
-            mensagem("sucess","Produtor cadastrado");
-            $idAtracao = recuperaUltimo("atracoes");
+        if (mysqli_query($con,$sqlInsert)){
+            $idProdutor = recuperaUltimo("produtores");
+            $sqlUpdate = "UPDATE `atracoes`
+                          SET produtor_id = '$idProdutor' 
+                          WHERE id ='$idAtracoes'";
+            if(mysqli_query($con,$sqlUpdate)){
+                mensagem("sucess","Produtor cadastrado");
+                $idAtracao = recuperaUltimo("atracoes");
+            }else{
+                mensagem("danger","Erro atrações");
+            }
         }
     }
-}
+
+
+    if (isset($_POST['edita'])){
+        $result = mysqli_query($con,$sqlSelect);
+        $sql  = "UPDATE `produtor`
+                 SET  nome = '$nome',
+                      email = '$email',
+                      telefone1 = '$telefone1',
+                      telefone2 = '$telefone2',
+                      observacao = '$observacao'
+                 WHERE id = ".$row['id'];
+        if (mysqli_query($con,$sql)){
+            mensagem("sucess","Cadastro atualizado com sucesso");
+        }
+        else{
+            mensagem("danger","Erro ao atualizar");
+        }
+
+    }
+
+    $row = recuperaDados("produtor","id", $idProdutor);
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -52,33 +74,47 @@ if (isset($_POST['cadastra'])){
                     <!-- form start -->
                     <form method="POST" action="?perfil=evento&p=produtor_edita" role="form">
                         <div class="box-body">
-
+                            <?php
+                            echo "<input type='hidden' name='idAtracoes' value='".$idAtracoes."''>";
+                            echo $row;
+                            ?>
                             <div class="form-group">
-                                <label for="nome">Nome</label>
-                                <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome completo" maxlength="120">
+                                <label for="nome">Nome: *</label>
+                                <?php
+                                    echo "<input type='text' class='form-control' id='nome' name='nome' maxlength='120' value='".$row['nome']."' required>";
+                                ?>
                             </div>
                             <div class="row">
                                 <div class="form-group col-md-6">
                                     <label for="email">E-mail</label>
-                                    <input type="email" class="form-control" id="email" name="email" maxlength="60" placeholder="Digite o e-mail">
+                                    <?php
+                                        echo "<input type='email' class='form-control' id='email' name='email' maxlength='60' placeholder='Digite o e-mail' value='".$row['email']."' required>";
+                                    ?>
+
                                 </div>
                                 <div class="form-group col-md-3">
                                     <label for="telefone1">Telefone #1</label>
-                                    <input type="text" class="form-control" id="telefone1" name="telefone1" maxlength="15" placeholder="Digite o Telefone principal">
+                                    <?php
+                                        echo "<input type='text' class='form-control' id='telefone1' name='telefone1' maxlength='15' placeholder='Digite o Telefone principal' required value='".$row['telefone1']."'>";
+                                    ?>
                                 </div>
                                 <div class="form-group col-md-3">
                                     <label for="telefone2">Telefone #2</label>
-                                    <input type="text" class="form-control" id="telefone2" name="telefone2" maxlength="15" placeholder="Digite o Telefone secundário">
+                                    <?php
+                                        echo "<input type='text' class='form-control' id='telefone2' name='telefone2' maxlength='15' placeholder='Digite o Telefone secundário' value='".$row['telefone2']."'>";
+                                    ?>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="observacao">Observação</label>
-                                <textarea name="observacao" id="observacao" class="form-control" rows="5"></textarea>
+                                <?php
+                                    echo "<textarea name='observacao' id='observacao' class='form-control' rows='5'>".$row['observacao']."</textarea>";
+                                ?>
                             </div>
                         </div>
                         <div class="box-footer">
                             <button type="submit" class="btn btn-default">Cancelar</button>
-                            <button type="submit" name="cadastra" class="btn btn-info pull-right">Cadastrar</button>
+                            <button type="submit" name="edita" class="btn btn-info pull-right">Alterar</button>
                         </div>
                     </form>
                 </div>
