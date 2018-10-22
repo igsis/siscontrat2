d22a80d3fc1b84449676e458d761fbf904942eea
Tanair <tanaiir@gmail.com>
2018-10-22 18:11:54 -0300

    tela especificidade teatro

--- perfil/evento/especificidade_teatro_lista.php
+++ perfil/evento/especificidade_teatro_lista.php
@@ -12,89 +12,44 @@ include "includes/menu_interno.php";
             <div class="col-md-12">
                 <div class="box box-info">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Informações Gerais</h3>
+                        <h3 class="box-title">Atração - Especificidades de Área</h3>
                     </div>
                     <form method="POST" action="?perfil=evento&p=evento_edita" role="form">
                         <div class="box-body">
-
                             <div class="row">
                                 <div class="form-group col-md-4">
-                                    <label for="original">É um evento original?</label> <br>
-                                    <label><input type="radio" name="original" value="1" checked> Sim </label>
-                                    <label><input type="radio" name="original" value="0"> Não </label>
+                                    <label for="estreia">Estréia?</label> <br>
+                                    <label><input type="radio" name="estreia" value="1" checked> Sim </label>
+                                    <label><input type="radio" name="estreia" value="0"> Não </label>
                                 </div>
                                 <div class="form-group col-md-4">
-                                    <label for="contratacao">Haverá contratação?</label> <br>
-                                    <label><input type="radio" name="contratacao" value="1" checked> Sim </label>
-                                    <label><input type="radio" name="contratacao" value="0"> Não </label>
+                                    <label for="gereno">Gênero</label><br/>
+                                    <input type="text" name="genero" size="30">
                                 </div>
-                                <div class="form-group col-md-4">
-                                    <label for="tipo">Tipo do Evento</label>
-                                    <select class="form-control" id="tipo" name="tipo" required>
-                                        <option value="">Selecione uma opção...</option>
-                                        <?php
-                                        geraOpcao("tipo_eventos", "");
-                                        ?>
-                                    </select>
-                                </div>
+                            <div class="form-group col-md-4">
+                                <label for="venda">Venda de material?</label> <br>
+                                <label><input type="radio" name="venda" value="1" checked> Sim </label>
+                                <label><input type="radio" name="venda" value="0"> Não </label>
+                            </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="nomeEvento">Nome do evento</label>
-                                <input type="text" class="form-control" id="nomeEvento" name="nomeEvento"
-                                       placeholder="Digite o nome do evento" maxlength="240" required>
-                            </div>
+
+
+
+
                             <div class="row">
-                                <div class="form-group col-md-6">
-                                    <label for="relacaoJuridica">Gênero</label>
-                                    <select class="form-control" name="relacaoJuridica" id="relacaoJuridica">
-                                        <option value="">Selecione uma opção...</option>
-                                        <?php
-                                        geraOpcao("relacao_juridicas", "");
-                                        ?>
-                                    </select>
-                                </div>
-
-                                <div class="form-group col-md-6">
-                                    <label for="projetoEspecial">Venda</label>
-                                    <class="form-control" id="Venda" name="venda">
-                                        <input type="number" value="valor">informe o preço da venda, se houver.</input>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
+                            <div class="form-group col-md-12">
                                 <label for="Descrição">Descrição</label><br/>
                                 <i>Esse campo deve conter uma breve descrição do que será apresentado no evento.</i>
-                                <p align="justify"><span style="color: gray; "><strong><i>Texto de exemplo:</strong><br/>Ana Cañas faz o show de lançamento do seu quarto disco, “Tô na Vida” (Som Livre/Guela Records). Produzido por Lúcio Maia (Nação Zumbi) em parceria com Ana e mixado por Mario Caldato Jr, é o primeiro disco totalmente autoral da carreira da cantora e traz parcerias com Arnaldo Antunes e Dadi entre outros.</span></i></p>
+                                <p align="justify"><span style="color: gray; "><strong><i>Texto de exemplo:</strong>Ana Cañas faz o show de lançamento do seu quarto disco, “Tô na Vida” (Som Livre/Guela Records). Produzido por Lúcio Maia (Nação Zumbi) em parceria com Ana e mixado por Mario Caldato Jr, é o primeiro disco totalmente autoral da carreira da cantora e traz parcerias com Arnaldo Antunes e Dadi entre outros.</span></i></p>
                                 <textarea name="sinopse" id="sinopse" class="form-control" rows="5"></textarea>
                             </div>
-
-                            <div class="row ">
-                                <div class="form-group col-md-6">
-                                    <label for="fiscal">Fiscal</label>
-                                    <select class="form-control" id="fiscal" name="fiscal">
-                                        <option value="">Selecione um fiscal...</option>
-                                        <?php
-                                        geraOpcaoUsuario("usuarios", 1, "");
-                                        ?>
-                                    </select>
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="suplente">Suplente</label>
-                                    <select class="form-control" id="suplente" name="suplente">
-                                        <option value="">Selecione um suplente...</option>
-                                        <?php
-                                        geraOpcaoUsuario("usuarios", 1, "");
-                                        ?>
-                                    </select>
-                                </div>
                             </div>
-
-                        </div>
-
                         <div class="box-footer">
                             <button type="submit" class="btn btn-default">Cancelar</button>
-                            <button type="submit" name="cadastra" class="btn btn-info pull-right">Cadastrar</button>
+                            <button type="submit" name="salvar" class="btn btn-info pull-right">Salvar</button>
+                        </div>
+
                         </div>
                     </form>
                 </div>
