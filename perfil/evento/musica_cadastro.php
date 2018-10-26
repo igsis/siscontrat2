17e4d0ff617f8e70c2e835537d7482d0d2b0959a
Tanair <tanaiir@gmail.com>
2018-10-26 15:28:53 -0300

    add página musica_cadastro

---
+++ perfil/evento/musica_cadastro.php
@@ -0,0 +1,47 @@
+<?php
+$con = bancoMysqli();
+include "includes/menu_interno.php";
+?>
+
+<div class="content-wrapper">
+    <section class="content">
+
+        <h2 class="page-header">Cadastro de Evento</h2>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-info">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Atração - Especificidades de Área</h3>
+                    </div>
+                    <form method="POST" action="?perfil=evento&p=evento_edita" role="form">
+                        <div class="box-body">
+                            <div class="row">
+                                <div class="form-group col-md-4">
+                                    <label for="gereno">Gênero</label><br/>
+                                    <input type="text" name="genero" size="30">
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="venda">Venda de material?</label> <br>
+                                    <label><input type="radio" name="venda" value="1" checked> Sim </label>
+                                    <label><input type="radio" name="venda" value="0"> Não </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="Descrição">Descrição</label><br/>
+                                    <i>Esse campo deve conter uma breve descrição do que será apresentado no evento.</i>
+                                    <textarea name="sinopse" id="sinopse" class="form-control" rows="5"></textarea>
+                                </div>
+                            </div>
+                            <div class="box-footer">
+                                <button type="submit" class="btn btn-default">Cancelar</button>
+                                <button type="submit" name="salvar" class="btn btn-info pull-right">Salvar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
