9ed77bb8c20aa65270682b86e89b256717fb0ba6
lucasdauto <31465480+lucasdauto@users.noreply.github.com>
2018-10-31 13:29:36 -0300

    Update perfil/evento/pf_cadastro_pesquisa.php

--- perfil/evento/pf_cadastro_pesquisa.php
+++ perfil/evento/pf_cadastro_pesquisa.php
@@ -50,7 +50,7 @@ if (isset($_POST['procurar'])){
                       </td>
                       <td>
                         <form method='post' action='?perfil=evento&p=pf_cadastro'>
-                            <input type='hidden' name='cpf-' value='$procurar'>
+                            <input type='hidden' name='documentacao' value='$procurar'>
                             <button class=\"btn btn-primary\" type='submit'>
                                 <i class=\"glyphicon glyphicon-plus\">        
                                 </i>Adicionar
