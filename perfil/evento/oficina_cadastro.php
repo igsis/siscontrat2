94a552498f4c56ecb296df13bdba145e51232e85
tanair <tanna@uninove.edu.br>
2018-11-02 22:24:46 -0300

    alterando para form-control

--- perfil/evento/oficina_cadastro.php
+++ perfil/evento/oficina_cadastro.php
@@ -36,7 +36,7 @@ $idAtracao = $_POST['idAtracao'];
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="vagas">Vagas</label> <br>
-                                    <input type="number" name="vagas">
+                                    <input class="form-control" style="max-width: 175px;" type="number" name="vagas">
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="venda">Venda de material?</label> <br>
@@ -53,39 +53,39 @@ $idAtracao = $_POST['idAtracao'];
                             <div class="row">
                                 <div class="form-group col-md-12">
                                     <label for="material">Material Requisitado: </label>
-                                    <textarea name="material" id="material" class="form-control" rows="2"></textarea>
+                                    <textarea  name="material" id="material" class="form-control" rows="3"></textarea>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="form-group col-md-4">
                                     <label for="inscricao">Forma de inscrição: </label><br>
-                                    <select id="inscricao" name="inscricao">
+                                    <select class="form-control" style="max-width: 175px;" id="inscricao" name="inscricao">
                                         <option value="">Selecione</option>
                                         <?php geraOpcao('forma_inscricao') ?>
                                     </select>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="carga_horaria">Carga Horária (em horas): </label><br>
-                                    <input type="number" name="carga_horaria"/>
+                                    <input class="form-control" style="max-width: 175px;" type="number" name="carga_horaria"/>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="valor_hora">Valor hora/aula: </label><br>
-                                    <input type="tel" name="valor_hora" placeholder="5,00" pattern="([0-9]{1,3}\.)?[0-9]{1,3},[0-9]{2}$">
+                                    <input class="form-control" style="max-width: 175px;" type="tel" name="valor_hora" placeholder="5,00" pattern="([0-9]{1,3}\.)?[0-9]{1,3},[0-9]{2}$">
                                 </div>
 
                             </div>
                             <div class="row">
                                 <div class="form-group col-md-4">
                                     <label for="inicio_inscricao">Início de inscrição: </label> <br/>
-                                    <input type="date" name="inicio_inscricao" onkeyup="barraData(this);"/>
+                                    <input class="form-control" style="max-width: 175px;" type="date" name="inicio_inscricao" onkeyup="barraData(this);"/>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="encerramento_inscricao">Encerramento de inscrição: </label> <br>
-                                    <input type="date" name="encerramento_inscricao"/>
+                                    <input class="form-control" style="max-width: 175px;" type="date" name="encerramento_inscricao"/>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="data_divulgacao">Divulgação de inscrição: </label> <br/>
-                                    <input type="date" name="data_divulgacao"/>
+                                    <input class="form-control" style="max-width: 175px;" type="date" name="data_divulgacao"/>
                                 </div>
                             </div>
                             <div class="box-footer">
