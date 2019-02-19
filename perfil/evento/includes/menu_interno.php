a95d73bb56e8b5f811af3691cc28ecb0b5fe616f
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-02-19 12:01:48 -0300

    Ajustes menu

--- perfil/evento/includes/menu_interno.php
+++ perfil/evento/includes/menu_interno.php
@@ -29,7 +29,7 @@ $pasta = "?perfil=evento&p=";
             ?>
             <li><a href="?perfil=evento"><i class="fa fa-circle-o"></i> <span>Voltar</span></a></li>
             <li class="header">MAIS</li>
-            <li><a href="../includes/ajuda.php"><i class="fa fa-user"></i><span>Minha Conta</span></a></li>
+            <li><a href="?perfil=usuario/minha_conta"><i class="fa fa-user"></i><span>Minha Conta</span></a></li>
             <li><a href="../includes/ajuda.php"><i class="fa fa-question "></i><span>Ajuda</span></a></li>
             <li><a href="../include/logoff.php"><i class="fa fa-sign-out"></i><span>Sair</span></a></li>
         </ul>
--- perfil/evento/includes/menu_principal.php
+++ perfil/evento/includes/menu_principal.php
@@ -17,7 +17,7 @@ $pasta = "?perfil=evento&p=";
             <li><a href="#"><i class="fa fa-circle-o"></i> <span>Buscar no CAPAC</span></a></li>
             <li><a href="#"><i class="fa fa-circle-o"></i> <span>Informações de produção</span></a></li>
             <li class="header">MAIS</li>
-            <li><a href="../include/ajuda.php"><i class="fa fa-user"></i><span>Minha Conta</span></a></li>
+            <li><a href="?perfil=usuario/minha_conta"><i class="fa fa-user"></i><span>Minha Conta</span></a></li>
             <li><a href="../include/ajuda.php"><i class="fa fa-question "></i><span>Ajuda</span></a></li>
             <li><a href="../include/logoff.php"><i class="fa fa-sign-out"></i><span>Sair</span></a></li>
         </ul>
