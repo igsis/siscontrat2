2eca0e9f782cd8204c60286061620843c03bcbc0
Teotonio Joaquim Soares Neto <teotonio.soares@fatec.sp.gov.br>
2019-03-12 12:26:39 -0300

    funcionalidade do botao APAGAR do lista_eventos

--- perfil/evento/evento_lista.php
+++ perfil/evento/evento_lista.php
@@ -6,13 +6,22 @@ unset($_SESSION['idPj']);
 unset($_SESSION['idPf']);
 
 $con = bancoMysqli();
+$conn = bancoPDO();
+
+if(isset($_POST['excluir'])){
+    $evento = $_POST['idEvent'];
+    $stmt = $conn->prepare("UPDATE eventos SET publicado = 0 WHERE id = :id");
+    $stmt->execute(['id' => $evento]);
+    $mensagem = mensagem("success", "Evento excluido com sucesso!");
+
+}
 
 $idUser = $_SESSION['idUser'];
 $sql = "SELECT ev.id AS idEvento, ev.nome_evento, te.tipo_evento, es.status FROM eventos AS ev
         INNER JOIN tipo_eventos AS te on ev.tipo_evento_id = te.id
         INNER JOIN evento_status es on ev.evento_status_id = es.id
         WHERE publicado = 1 AND (usuario_id = '$idUser' OR fiscal_id = '$idUser' OR suplente_id = '$idUser') AND evento_status_id = 1";
-$query = mysqli_query($con,$sql);
+$query = mysqli_query($con, $sql);
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -44,20 +53,28 @@ $query = mysqli_query($con,$sql);
 
                             <?php
                             echo "<tbody>";
-                            while ($evento = mysqli_fetch_array($query)){
+                            while ($evento = mysqli_fetch_array($query)) {
                                 echo "<tr>";
-                                echo "<td>".$evento['nome_evento']."</td>";
-                                echo "<td>".$evento['tipo_evento']."</td>";
-                                echo "<td>".$evento['status']."</td>";
+                                echo "<td>" . $evento['nome_evento'] . "</td>";
+                                echo "<td>" . $evento['tipo_evento'] . "</td>";
+                                echo "<td>" . $evento['status'] . "</td>";
                                 echo "<td>
                                     <form method=\"POST\" action=\"?perfil=evento&p=evento_edita\" role=\"form\">
-                                    <input type='hidden' name='idEvento' value='".$evento['idEvento']."'>
+                                    <input type='hidden' name='idEvento' value='" . $evento['idEvento'] . "'>
                                     <button type=\"submit\" name='carregar' class=\"btn btn-block btn-primary\"><span class='glyphicon glyphicon-eye-open'></span></button>
                                     </form>
                                 </td>";
-                                echo "<td>
-                    <button type=\"button\" class=\"btn btn-block btn-danger\"><span class='glyphicon glyphicon-trash'></span></button>
-                  </td>";
+                                ?>
+                                <td>
+                                    <form method="post" id="formExcluir">
+                                        <input type="hidden" name="idEvento" value="<?= $evento['id'] ?>">
+                                        <button type="button" class="btn btn-block btn-danger" id="excluiEvento"
+                                                data-toggle="modal" data-target="#exclusao" name="excluiEvento"
+                                                data-name="<?= $evento['nome_evento'] ?>"
+                                                data-id="<?= $evento['idEvento'] ?>"><span class="glyphicon glyphicon-trash"></span></button>
+                                    </form>
+                                </td>
+                                <?php
                                 echo "</tr>";
                             }
                             echo "</tbody>";
@@ -81,6 +98,31 @@ $query = mysqli_query($con,$sql);
         <!-- /.row -->
         <!-- END ACCORDION & CAROUSEL-->
 
+        <!--.modal-->
+        <div id="exclusao" class="modal modal-danger modal fade in" role="dialog">
+            <div class="modal-dialog">
+                <!--Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Confirmação de Exclusão</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Tem certeza que deseja excluir este evento?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <form action="?perfil=evento&p=evento_lista" method="post">
+                            <input type="hidden" name="idEvent" id="idEvent" value="">
+                            <input type="hidden" name="apagar" id="apagar">
+                            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancelar
+                            </button>
+                            <input class=" btn btn-danger btn-outline" type="submit" name="excluir" value="Apagar">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </section>
     <!-- /.content -->
 </div>
@@ -89,15 +131,24 @@ $query = mysqli_query($con,$sql);
 <script defer src="../visual/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 
 <script type="text/javascript">
-        $(function () {
+    $(function () {
         $('#tblEvento').DataTable({
             "language": {
                 "url": 'bower_components/datatables.net/Portuguese-Brasil.json'
             },
             "responsive": true,
             "dom": "<'row'<'col-sm-6'l><'col-sm-6 text-right'f>>" +
-                "<'row'<'col-sm-12'tr>>" +
-                "<'row'<'col-sm-5'i><'col-sm-7 text-right'p>>",
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'i><'col-sm-7 text-right'p>>",
         });
     });
+</script>
+<script type="text/javascript">
+    $('#exclusao').on('show.bs.modal', function (e){
+        let evento = $(e.relatedTarget).attr('data-name');
+        let id = $(e.relatedTarget).attr('data-id');
+
+        $(this).find('p').text(`Tem certeza que deseja excluir o evento ${evento} ?`);
+        $(this).find('#idEvent').attr('value', `${id}`);
+    })
 </script>
