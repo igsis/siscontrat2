e742b94cf9a5c7202cafda3e689b7986c3ba387e
Lorelei Gabriele <loreleigab@hotmail.com>
2019-03-26 21:38:42 -0300

    Ajuste no menu administrativo

--- perfil/administrativo/includes/menu.php
+++ perfil/administrativo/includes/menu.php
@@ -6,18 +6,13 @@
         <ul class="sidebar-menu" data-widget="tree">
             <li><a href="?secao=perfil"><i class="fa fa-home"></i><span>Home</span></a></li>
             <li class="header">MÓDULO ADMINISTRATIVO</li>
-
             <li><a href="?perfil=administrativo&p=perfil&sp=perfil_lista"><i class="fa fa-circle-o text-purple"></i><span>Perfil</span></a></li>
             <li><a href="?perfil=administrativo&p=usuario&sp=usuario_lista"><i class="fa fa-circle-o text-red"></i><span>Usuários</span></a></li>
             <li><a href="?perfil=administrativo&p=projeto_especial&sp=projeto_especial_lista"><i class="fa fa-circle-o text-blue"></i><span>Projeto Especial</span></a></li>
             <li><a href="?perfil=administrativo&p=modulos&sp=modulos_lista"><i class="fa fa-circle-o text-aqua"></i><span>Gerenciar modulos</span></a></li>
-
-
-            <li class="header">MAIS</li>
-
-            <li><a href="?perfil=usuario/minha_conta"><i class="fa fa-user"></i><span>Minha Conta</span></a></li>
-            <li><a href="../manuais/MANUAL_MODULO_ADMINISTRATIVO.pdf" target="_blank"><i class="fa fa-question "></i><span>Ajuda</span></a></li>
-            <li><a href="../include/logoff.php"><i class="fa fa-sign-out"></i><span>Sair</span></a></li>
+            <?php
+            include "../perfil/includes/menu_mais.php";
+            ?>
         </ul>
     </section>
     <!-- /.sidebar -->
