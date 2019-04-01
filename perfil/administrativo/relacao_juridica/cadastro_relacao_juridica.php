b09318c01e328350f26e7a3dbafa3dfeb73885bf
GiovannaGeorgetti <giovannamirellegeorgetti@gmail.com>
2019-04-01 14:19:06 -0300

    Cadastro de Relaçao juridica

---
+++ perfil/administrativo/relacao_juridica/cadastro_relacao_juridica.php
@@ -0,0 +1,54 @@
+<div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- START FORM-->
+        <h2 class="page-header">Cadastro de Relação Jurídica</h2>
+
+        <div class="row">
+            <div class="col-md-12">
+                <!-- general form elements -->
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Relação Jurídica</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                    <form method="POST" action="?perfil=administrativo&p=relacao_juridica&sp=edita_relacao_juridica"
+                          role="form">
+                        <div class="box-body">
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="titulo">Título *</label>
+                                    <input align="center" type="text" id="titulo" name="titulo" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="form-group  col-md-12">
+                                    <label for="msg">Mensagem *</label>
+                                    <textarea type="text" rows="5" id="msg" name="msg" class="form-control" required></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+
+                        <div class="box-footer">
+                            <a href="?perfil=administrativo&p=relacao_juridica&sp=relacao_juridica_lista">
+                                <button type="button" class="btn btn-default">Voltar</button>
+                            </a>
+                            <button type="submit" name="cadastra" id="cadastra" class="btn btn-primary pull-right">
+                                Cadastrar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+        <!-- END ACCORDION & CAROUSEL-->
+
+    </section>
+    <!-- /.content -->
+</div>
