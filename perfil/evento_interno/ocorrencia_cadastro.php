0c1776334b98a856e40d25c3702f1661c5db86de
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-04-03 16:00:13 -0300

    Ocorrencia de evento interno com virada cultural

--- perfil/evento_interno/ocorrencia_cadastro.php
+++ perfil/evento_interno/ocorrencia_cadastro.php
@@ -1,7 +1,7 @@
 <?php
 $con = bancoMysqli();
 include "includes/menu_interno.php";
-$url = 'http://'.$_SERVER['HTTP_HOST'].'/siscontrat2/funcoes/api_locais_espacos.php';
+$url = 'http://' . $_SERVER['HTTP_HOST'] . '/siscontrat2/funcoes/api_locais_espacos.php';
 
 $_SESSION['idOrigem'] = $_POST['idOrigem'];
 
@@ -38,11 +38,10 @@ $evento = recuperaDados('eventos', 'id', $idEvento);
     });
 
     function validate() {
-        comparaData(); 
+        comparaData();
         if ($('#datepicker11').val().length > 0) {
             mudaData(false);
-        }
-        else {
+        } else {
             mudaData(true);
 
             var data = document.querySelector('input[name="data_inicio"]').value;
@@ -75,24 +74,24 @@ $evento = recuperaDados('eventos', 'id', $idEvento);
         }
     }
 
-    function comparaData(){
+    function comparaData() {
         var dataInicio = document.querySelector('#datepicker10').value;
         var dataFim = document.querySelector('#datepicker11').value;
 
 
         var dataInicio = parseInt(dataInicio.split("-")[0].toString() + dataInicio.split("-")[1].toString() + dataInicio.split("-")[2].toString());
-        if(dataFim != ""){
+        if (dataFim != "") {
             var dataFim = parseInt(dataFim.split("-")[0].toString() + dataFim.split("-")[1].toString() + dataFim.split("-")[2].toString());
 
-            if(dataFim <= dataInicio){
+            if (dataFim <= dataInicio) {
                 alert("Data final menor que a data inicial");
                 $('#cadastra').attr("disabled", true);
-            }else{
+            } else {
                 $('#cadastra').attr("disabled", false);
             }
         }
 
-        if(dataFim == ""){
+        if (dataFim == "") {
             $('#cadastra').attr("disabled", false);
         }
 
@@ -129,7 +128,7 @@ $evento = recuperaDados('eventos', 'id', $idEvento);
                             </div>
 
                             <div class="row">
-                                <div class="form-group col-md-12">
+                                <div class="form-group col-md-6">
                                     <label>
                                         <input type="checkbox" name="domingo" id="diasemana07" value="1"> Domingo &nbsp;
                                         <input type="checkbox" name="segunda" id="diasemana01" value="1"> Segunda &nbsp;
@@ -139,6 +138,14 @@ $evento = recuperaDados('eventos', 'id', $idEvento);
                                         <input type="checkbox" name="sexta" id="diasemana05" value="1"> Sexta &nbsp;
                                         <input type="checkbox" name="sabado" id="diasemana06" value="1"> Sábado &nbsp;
                                     </label>
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label for="virada">É virada?</label> &nbsp;
+                                    <input type="radio" name="virada" id="viradaSim" value="1" class="virada"> Sim
+                                    &nbsp;
+                                    <input type="radio" name="virada" id="viradaNao" value="0" checked class="virada">
+                                    Não
                                 </div>
                             </div>
 
@@ -208,9 +215,13 @@ $evento = recuperaDados('eventos', 'id', $idEvento);
                         </div>
 
                         <div class="box-footer">
-                            <a href="?perfil=evento_interno&p=ocorrencia_lista"><button type="button" class="btn btn-default" id="voltar" name="voltar">Voltar</button></a>
+                            <a href="?perfil=evento_interno&p=ocorrencia_lista">
+                                <button type="button" class="btn btn-default" id="voltar" name="voltar">Voltar</button>
+                            </a>
                             <input type="hidden" name="idOrigem" value="<?= $_POST['idOrigem'] ?>">
-                            <button type="submit" name="cadastra" id="cadastra" class="btn btn-info pull-right">Cadastrar</button>
+                            <button type="submit" name="cadastra" id="cadastra" class="btn btn-info pull-right">
+                                Cadastrar
+                            </button>
                         </div>
                     </form>
                 </div>
@@ -220,51 +231,163 @@ $evento = recuperaDados('eventos', 'id', $idEvento);
 </div>
 
 <script>
-   const url = `<?=$url?>`;
+    const url = `<?=$url?>`;
 
-   let instituicao = document.querySelector('#instituicao');
+    let instituicao = document.querySelector('#instituicao');
 
     instituicao.addEventListener('change', async e => {
         let idInstituicao = $('#instituicao option:checked').val();
 
         fetch(`${url}?instituicao_id=${idInstituicao}`)
             .then(response => response.json())
-            .then(locais => {                
+            .then(locais => {
                 $('#local option').remove();
                 $('#local').append('<option value="">Selecione uma opção...</option>');
 
                 for (const local of locais) {
-                    $('#local').append(`<option value='${local.id}'>${local.local}</option>`).focus();;
-                }                
+                    $('#local').append(`<option value='${local.id}'>${local.local}</option>`).focus();
+                    ;
+                }
             })
 
     })
-    
+
     let local = document.querySelector('#local');
 
-    local.addEventListener('change', async e => {        
+    local.addEventListener('change', async e => {
         let idLocal = $('#local option:checked').val();
-        
+
         fetch(`${url}?espaco_id=${idLocal}`)
-            .then(response => response.json() )
+            .then(response => response.json())
             .then(espacos => {
                 $('#espaco option').remove();
-                if(espacos.length < 1){
+                if (espacos.length < 1) {
                     $('#espaco').append('<option value="">Não há espaço para esse local</option>')
-                    .attr('required',false)
-                    .focus();
-                }else{
+                        .attr('required', false)
+                        .focus();
+                } else {
                     $('#espaco').append('<option value="">Selecione uma opção...</option>')
-                    .attr('required',true)
-                    .focus();
+                        .attr('required', true)
+                        .focus();
                 }
-                
+
                 for (const espaco of espacos) {
                     $('#espaco').append(`<option value='${espaco.id}'>${espaco.espaco}</option>`)
                 }
-             
+
             })
     })
+
+
+    var virada = $('.virada');
+
+    virada.on("change", function () {
+        if ($('#viradaNao').is(':checked')) {
+            $('#horaInicio')
+                .attr('readonly', false)
+                .val('');
+
+            $('#horaFim')
+                .attr('readonly', false)
+                .val('');
+
+            $('#instituicao')
+                .attr('readonly', false)
+                .val($('option:contains("Selecione uma opção...")').val());
+
+            $('#local')
+                .attr('readonly', false)
+                .val($('option:contains("Selecione uma opção...")').val());
+
+            $('#espaco')
+                .attr('readonly', false)
+                .val($('option:contains("Selecione uma opção...")').val());
+
+            $('#retiradaIngresso')
+                .attr('readonly', false)
+                .val($('option:contains("Selecione uma opção...")').val());
+
+            $('#valor_ingresso')
+                .attr('readonly', false)
+                .val('');
+        } else {
+            $('#horaInicio')
+                .attr('readonly', true)
+                .val('00:00');
+
+            $('#horaFim')
+                .attr('readonly', true)
+                .val('00:00');
+
+            $('#instituicao')
+                .attr('readonly', true)
+                .val($('option:contains("Virada Cultural")').val());
+
+            $('#local')
+                .attr('readonly', true)
+                .val($('option:contains("De acordo com a programação do evento")').val());
+
+            $('#retiradaIngresso')
+                .attr('readonly', true)
+                .val($('option:contains("INGRESSOS GRÁTIS")').val());
+
+            $('#espaco')
+                .attr('readonly', true);
+
+            $('#valor_ingresso')
+                .attr('readonly', true)
+                .val('0,00');
+        }
+
+        getLocais(10, 626);
+        getEspacos();
+    });
+
+    function getLocais(idInstituicao, selectedId) {
+        fetch(`${url}?instituicao_id=${idInstituicao}`)
+            .then(response => response.json())
+            .then(locais => {
+                $('#local option').remove();
+                $('#local').append('<option value="">Selecione uma opção...</option>');
+
+                for (const local of locais) {
+                    if (selectedId == local.id) {
+                        $('#local').append(`<option value='${local.id}' selected>${local.local}</option>`).focus();
+                        ;
+                    } else {
+                        $('#local').append(`<option value='${local.id}'>${local.local}</option>`).focus();
+                        ;
+                    }
+
+                }
+            })
+    }
+
+    function getEspacos(idLocal, selectedId) {
+        fetch(`${url}?espaco_id=${idLocal}`)
+            .then(response => response.json())
+            .then(espacos => {
+                $('#espaco option').remove();
+                if (espacos.length < 1) {
+                    $('#espaco').append('<option value="">Não há espaço para esse local</option>')
+                        .attr('required', false)
+                        .focus();
+                } else {
+                    $('#espaco').append('<option value="">Selecione uma opção...</option>')
+                        .attr('required', true)
+                        .focus();
+                }
+
+                for (const espaco of espacos) {
+                    if (selectedId == espaco.id) {
+                        $('#espaco').append(`<option value='${espaco.id}' selected>${espaco.espaco}</option>`)
+                    } else {
+                        $('#espaco').append(`<option value='${espaco.id}'>${espaco.espaco}</option>`)
+                    }
+                }
+
+            })
+    }
 
 </script>
 
