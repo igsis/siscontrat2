d430b3baf31e59be93f00a8c785d50144d5dd43d
Tannair <tanaiiir@gmail.com>
2019-04-16 17:08:21 -0300

    Update solicitar_espaco.php

--- perfil/evento/solicitar_espaco.php
+++ perfil/evento/solicitar_espaco.php
@@ -1,5 +1,6 @@
 <?php
 include "includes/menu_principal.php";
+$url = 'http://' . $_SERVER['HTTP_HOST'] . '/siscontrat2/funcoes/api_locais_espacos.php';
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -24,15 +25,23 @@ include "includes/menu_principal.php";
                         <div class="box-body">
                             <div class="row">
                                 <div class="form-group col-md-6">
-                                    <label for="local">Local: </label>
-                                    <select name="local" id="local" class="form-control" required>
+                                    <label for="instituicao">Instituição: *</label>
+                                    <select name="instituicao" id="instituicao" class="form-control" required>
                                         <?php
-                                            geraOpcaoPublicado('locais');
+                                        geraOpcao('instituicoes');
                                         ?>
                                     </select>
                                 </div>
-
                                 <div class="form-group col-md-6">
+                                    <label for="local">Local: </label>
+                                    <select name="local" id="local" class="form-control" required>
+                                       <!-- -
+/*                                            geraOpcaoPublicado('locais');
+                                        */?> -->
+                                    </select>
+                                </div>
+
+                                <div class="form-group col-md-12">
                                     <label for="sigla">Espaço: </label>
                                     <input type="text" class="form-control" id="espaco" name="espaco" required>
                                 </div>
@@ -59,3 +68,145 @@ include "includes/menu_principal.php";
     </section>
     <!-- /.content -->
 </div>
+
+
+
+
+
+<script type="text/javascript">
+
+    const url = `<?=$url?>`;
+
+    let instituicao = document.querySelector('#instituicao');
+
+    instituicao.addEventListener('change', async e => {
+        let idInstituicao = $('#instituicao option:checked').val();
+
+        console.log(instituicao);
+
+        fetch(`${url}?instituicao_id=${idInstituicao}`)
+            .then(response => response.json())
+            .then(locais => {
+                $('#local option').remove();
+                $('#local').append('<option value="">Selecione uma opção...</option>');
+
+                for (const local of locais) {
+                    $('#local').append(`<option value='${local.id}'>${local.local}</option>`).focus();
+                    ;
+                }
+            })
+    })
+
+    let local = document.querySelector('#local');
+
+    local.addEventListener('change', async e => {
+        let idLocal = $('#local option:checked').val();
+
+        fetch(`${url}?espaco_id=${idLocal}`)
+            .then(response => response.json())
+            .then(espacos => {
+                $('#espaco option').remove();
+                if (espacos.length < 1) {
+                    $('#espaco').append('<option value="">Não há espaço para esse local</option>')
+                        .attr('required', false)
+                        .focus();
+                } else {
+                    $('#espaco').append('<option value="">Selecione uma opção...</option>')
+                        .attr('required', true)
+                        .focus();
+                }
+
+                for (const espaco of espacos) {
+                    $('#espaco').append(`<option value='${espaco.id}'>${espaco.espaco}</option>`)
+                }
+
+            })
+    })
+
+    var virada = $('.virada');
+
+    virada.on("change", function () {
+        if ($('#viradaNao').is(':checked')) {
+            $('#horaInicio')
+                .attr('readonly', false)
+                .val('');
+
+            $('#horaFim')
+                .attr('readonly', false)
+                .val('');
+
+            $('#instituicao')
+                .attr('readonly', false)
+                .val($('option:contains("Selecione uma opção...")').val());
+
+            $('#local')
+                .attr('readonly', false)
+                .val($('option:contains("Selecione uma opção...")').val());
+
+            $('#espaco')
+                .attr('readonly', false)
+                .val($('option:contains("Selecione uma opção...")').val());
+
+            $('#retiradaIngresso')
+                .attr('readonly', false)
+                .val($('option:contains("Selecione uma opção...")').val());
+
+            $('#valor_ingresso')
+                .attr('readonly', false)
+                .val('');
+        } else {
+            $('#horaInicio')
+                .attr('readonly', true)
+                .val('00:00');
+
+            $('#horaFim')
+                .attr('readonly', true)
+                .val('00:00');
+
+            $('#instituicao')
+                .attr('readonly', true)
+                .val($('option:contains("Virada Cultural")').val());
+
+            $('#local')
+                .attr('readonly', true)
+                .val($('option:contains("De acordo com a programação do evento")').val());
+
+            $('#retiradaIngresso')
+                .attr('readonly', true)
+                .val($('option:contains("INGRESSOS GRÁTIS")').val());
+
+            $('#espaco')
+                .attr('readonly', true);
+
+            $('#valor_ingresso')
+                .attr('readonly', true)
+                .val('0,00');
+        }
+
+        getLocais(10, 626);
+        getEspacos();
+    });
+
+    function getLocais(idInstituicao, selectedId) {
+        fetch(`${url}?instituicao_id=${idInstituicao}`)
+            .then(response => response.json())
+            .then(locais => {
+                $('#local option').remove();
+                $('#local').append('<option value="">Selecione uma opção...</option>');
+
+                for (const local of locais) {
+                    if (selectedId == local.id) {
+                        $('#local').append(`<option value='${local.id}' selected>${local.local}</option>`).focus();
+                        ;
+                    } else {
+                        $('#local').append(`<option value='${local.id}'>${local.local}</option>`).focus();
+                        ;
+                    }
+
+                }
+            })
+    }
+
+</script>
+
+
