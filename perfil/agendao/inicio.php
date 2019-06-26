7eba6bf25f745f43c79a7028d2a79a92f91de7a4
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-06-26 15:57:48 -0300

    Revert "Revert "Merge branch 'master' of https://github.com/igsis/siscontrat2""
    This reverts commit d7edf5c82b8815af79644c5f720c0fd62e3adda2.

--- perfil/agendao/inicio.php
+++ perfil/agendao/inicio.php
@@ -1,89 +1,30 @@
 <?php
 include "includes/menu_principal.php";
-
-$con = bancoMysqli();
-
-unset($_SESSION['idEvento']);
-unset($_SESSION['idPj']);
-unset($_SESSION['idPf']);
-
-$idUsuario = $_SESSION['idUser'];
-
-
-$idUser = $_SESSION['idUser'];
-$sql = "SELECT * FROM eventos WHERE publicado = 1 AND agendao = 1 AND evento_status_id >= 3 AND (suplente_id = '$idUsuario' OR fiscal_id = '$idUsuario' OR usuario_id = '$idUsuario') ORDER BY id DESC LIMIT 0,20";
-
-$query = mysqli_query($con, $sql);
-$linha = mysqli_num_rows($query);
-
-if ($linha >= 1) {
-    $tem = 1;
-} else {
-    $tem = 0;
-}
 ?>
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Main content -->
     <section class="content">
-        <!-- START ACCORDION-->
-        <h2 class="page-header">Seus últimos eventos internos enviados</h2>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="box box-solid">
-                    <div class="box-body">
-                        <div class="box-group" id="accordionEventoInterno">
-                            <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
-                            <?php
-                            if ($tem == 0) {
-                                $mensagem = mensagem("info", "Não existe eventos internos enviados!");
-                            } else {
-                                while ($evento = mysqli_fetch_array($query)) {
-                                    $locais = listaLocais($evento['id'], '1');
-                                    ?>
-                                    <div class="panel box box-primary">
-                                        <div class="box-header with-border">
-                                            <h4 class="box-title">
-                                                <a data-toggle="collapse" data-parent="#accordionEventoInterno"
-                                                   href="#collapse<?=$evento['id']?>">
-                                                    <?= $evento['nome_evento'] ?>
-                                                </a>
-                                            </h4>
-                                        </div>
-                                        <div id="collapse<?=$evento['id']?>" class="panel-collapse collapse">
-                                            <div class="box-body">
-                                                <?php
-                                                $dataEnvio = recuperaDados('evento_envios', 'evento_id', $evento['id']);
-                                                $usuario = recuperaDados('usuarios', 'id', $evento['usuario_id']);
-                                                ?>
-                                                <p><b>Enviado por: </b><?= $usuario['nome_completo'] ?>
-                                                    <b>em:</b> <?= exibirDataBr($dataEnvio['data_envio']) ?> </p>
-                                                <p><b>Período:</b> <?= retornaPeriodoNovo($evento['id']) ?> </p>
-                                                <p><b>Local:</b> <?= $locais ?></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <?php
-                                }
-                            }
-                            ?>
-                            <div class="row" align="center">
-                                <?php if (isset($mensagem)) {
-                                    echo $mensagem;
-                                }; ?>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
+        <div class="box box-widget widget-user">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-lime-active">
+                <h3 class="widget-user-username"><span style="color: black;"><b>Módulo Agendão</b></span></h3>
             </div>
-            <!-- /.col -->
+            <div class="box-footer">
+                <div class="row">
+                    <div class="col-md-12 border-right">
+                        <div class="description-block">
+                            <span class="description-header">
+                                Nesse módulo é possível inserir e gerenciar eventos sem necessidade de contratação artística e que ainda não constem no sistema, editá-los e disponibilizá-los para importação no site Agendão.
+                                <br/><br/>
+                            </span>
+                        </div>
+                        <!-- /.description-block -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
         </div>
-        <!-- /.row -->
-        <!-- END ACCORDION & CAROUSEL-->
-
     </section>
-    <!-- /.content -->
 </div>
