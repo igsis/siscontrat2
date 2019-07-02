e071dacbbecabe4f14f94db7d09e03f94ef3e707
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-07-02 16:40:46 -0300

    Removendo o botão de excluir modulos

--- perfil/administrativo/modulos/modulos_lista.php
+++ perfil/administrativo/modulos/modulos_lista.php
@@ -2,13 +2,6 @@
 $con = bancoMysqli();
 $conn = bancoPDO();
 
-if (isset($_POST['excluir'])) {
-    $idModulo= $_POST['idModulo'];
-    $stmt = $conn->prepare("DELETE FROM `modulos` WHERE id = :id");
-    $stmt->execute(['id' => $idModulo]);
-    $mensagem = mensagem("success", "Módulo excluído com sucesso!");
-}
-
 $sql = "SELECT * FROM modulos";
 $query = mysqli_query($con, $sql);
 ?>
@@ -41,7 +34,6 @@ $query = mysqli_query($con, $sql);
                             <tr>
                                 <th width="95%">Módulo</th>
                                 <th>Visualizar</th>
-                                <th>Excluir</th>
                             </tr>
                             </thead>
 
@@ -56,18 +48,6 @@ $query = mysqli_query($con, $sql);
                                     <button type='submit' name='carregar' class='btn btn-block btn-primary'><span class='glyphicon glyphicon-eye-open'></span></button>
                                     </form>
                                 </td>";
-                                ?>
-                                <td>
-                                    <form method='POST' id='formExcliuir'>
-                                        <input type="hidden" name='idModulo' value="<?= $modulo['id'] ?>">
-                                        <button type="button" class="btn btn-block btn-danger" id="excluiModulo"
-                                                data-toggle="modal" data-target="#exclusao" name="excluirModulo"
-                                                data-nome="<?= $modulo['descricao'] ?>"
-                                                data-id="<?= $modulo['id'] ?>"><span
-                                                    class='glyphicon glyphicon-trash'></span></button>
-                                    </form>
-                                </td>
-                                <?php
                             }
                             echo "</tbody>";
                             ?>
@@ -75,7 +55,6 @@ $query = mysqli_query($con, $sql);
                             <tr>
                                 <th>Módulo</th>
                                 <th>Visualizar</th>
-                                <th>Excluir</th>
                             </tr>
                             </tfoot>
                         </table>
@@ -88,33 +67,6 @@ $query = mysqli_query($con, $sql);
         </div>
         <!-- /.row -->
         <!-- END ACCORDION & CAROUSEL-->
-
-        <!--.modal-->
-        <div id="exclusao" class="modal modal-danger modal fade in" role="dialog">
-            <div class="modal-dialog">
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Confirmação de Exclusão</h4>
-                    </div>
-                    <div class="modal-body">
-                        <p>Tem certeza que deseja excluir este usuário?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <form action="?perfil=administrativo&p=modulos&sp=modulos_lista" method="post">
-                            <input type="hidden" name="idModulo" id="idModulo" value="">
-                            <input type="hidden" name="apagar" id="apagar">
-                            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancelar
-                            </button>
-                            <input class="btn btn-danger btn-outline" type="submit" name="excluir" value="Apagar">
-                        </form>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
     </section>
     <!-- /.content -->
 </div>
