40df8aa362a93fa36f7017bd31a40c869758540d
Lorelei Gabriele <loreleigab@hotmail.com>
2019-07-04 16:27:58 -0300

    Finalizar e valida~]ao do agendão

--- perfil/agendao/includes/validacoes.php
+++ perfil/agendao/includes/validacoes.php
@@ -2,40 +2,8 @@
 $con = bancoMysqli();
 
 $idEvento = $_SESSION['idEvento'];
-
 $evento = recuperaDados('agendoes', 'id', $idEvento);
 
-/**
- * <p>Recebe um array multidimensional para ser utilizado na função in_array()</p>
- *
- * @param string|int $needle <p>
- * Valor a ser procurado </p>
- * @param array $haystack <p>
- * Array multidimensional onde deve procurar
- * @return array <p>
- * Retorna um array contendo os indices: <br>
- * 'bool' - false ou true <br>
- * 'especificidade' - indice do array multidimencional onde foi encontrado o valor </p>
- */
-/*
-function in_array_key($needle, $haystack) {
-    $return = [
-        'bool' => false,
-        'especificidade' => null
-    ];
-
-    foreach ($haystack as $key => $array) {
-        if (in_array($needle, $array)) {
-            $return = [
-                'bool' => true,
-                'especificidade' => $key
-            ];
-            return $return;
-        }
-    }
-    return $return;
-}
-*/
 $erros = [];
 
 if (($evento['produtor_id'] == "") || ($evento['produtor_id'] == null)) {
