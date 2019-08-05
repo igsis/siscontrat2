4a1869e473c7984abdac3336b7b787e73263bc12
Lorelei Gabriele <loreleigab@hotmail.com>
2019-08-05 15:46:14 -0300

    Ajuste do status do evento no sql de Gestão de Prazos

--- perfil/gestao_prazo/index.php
+++ perfil/gestao_prazo/index.php
@@ -13,7 +13,7 @@ $sql = "SELECT
                INNER JOIN pedidos AS p ON p.origem_id = e.id 
                INNER JOIN ocorrencias AS o ON o.origem_ocorrencia_id = e.id
                INNER JOIN locais AS l ON l.id = o.local_id
-               WHERE evento_status_id = 3 AND e.publicado = 1 AND p.status_pedido_id = 1";
+               WHERE evento_status_id = 2 AND e.publicado = 1 AND p.status_pedido_id = 1";
 $query = mysqli_query($con,$sql);
 $numEventos = mysqli_num_rows($query);
 ?>
