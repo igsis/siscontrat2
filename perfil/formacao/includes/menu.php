229e5731cd28e48e546d8be52d40a91cd24651ee
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-08-22 15:47:38 -0300

    pesquisa, cadastro e edição de pessoa fisica

--- perfil/formacao/includes/menu.php
+++ perfil/formacao/includes/menu.php
@@ -16,7 +16,7 @@ $pasta = "?perfil=formacao&p=";
                     <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Cadastrar</a></li>
+                    <li><a href="<?= $pasta ?>pessoa_fisica&sp=pesquisa"><i class="fa fa-circle-o"></i> Cadastrar</a></li>
                     <li><a href="#"><i class="fa fa-circle-o"></i> Importar do CAPAC</a></li>
                     <li><a href="#"><i class="fa fa-circle-o"></i> Listar</a></li>
                 </ul>
