b0f1a5b64ae7221a88c225e1a0a4f065ef22cd2f
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-09-11 12:58:11 -0300

    cadastrando valor = 0 tbm

--- perfil/evento/pj_cadastro.php
+++ perfil/evento/pj_cadastro.php
@@ -4,7 +4,7 @@ $con = bancoMysqli();
 $idEvento = $_SESSION['idEvento'];
 
 $sql = "SELECT valor_individual FROM atracoes WHERE evento_id = '$idEvento'";
-$atracao = mysqli_query($con,$sql);
+$atracao = mysqli_query($con, $sql);
 ?>
 
 <script>
@@ -48,26 +48,33 @@ $atracao = mysqli_query($con,$sql);
                             <div class="row">
                                 <div class="form-group col-md-6">
                                     <label for="email">E-mail: *</label>
-                                    <input type="email" name="email" class="form-control" maxlength="60" placeholder="Digite o E-mail" required>
+                                    <input type="email" name="email" class="form-control" maxlength="60"
+                                           placeholder="Digite o E-mail" required>
                                 </div>
                                 <div class="form-group col-md-2">
                                     <label>Telefone #1: *</label>
-                                    <input type="text" id="telefone" name="telefone[0]" onkeyup="mascara( this, mtel );"  class="form-control" placeholder="Digite o telefone" required maxlength="15">
+                                    <input type="text" id="telefone" name="telefone[0]" onkeyup="mascara( this, mtel );"
+                                           class="form-control" placeholder="Digite o telefone" required maxlength="15">
                                 </div>
                                 <div class="form-group col-md-2">
                                     <label>Telefone #2:</label>
-                                    <input type="text" id="telefone1" name="telefone[1]" onkeyup="mascara( this, mtel );"  class="form-control" placeholder="Digite o telefone" maxlength="15">
+                                    <input type="text" id="telefone1" name="telefone[1]"
+                                           onkeyup="mascara( this, mtel );" class="form-control"
+                                           placeholder="Digite o telefone" maxlength="15">
                                 </div>
                                 <div class="form-group col-md-2">
                                     <label>Telefone #3:</label>
-                                    <input type="text" id="telefone2" name="telefone[2]" onkeyup="mascara( this, mtel );"  class="form-control telefone" placeholder="Digite o telefone" maxlength="15">
+                                    <input type="text" id="telefone2" name="telefone[2]"
+                                           onkeyup="mascara( this, mtel );" class="form-control telefone"
+                                           placeholder="Digite o telefone" maxlength="15">
                                 </div>
                             </div>
                             <hr/>
                             <div class="row">
                                 <div class="form-group col-md-3">
                                     <label for="cep">CEP: *</label>
-                                    <input type="text" class="form-control" name="cep" id="cep" maxlength="9" placeholder="Digite o CEP" required data-mask="00000-000">
+                                    <input type="text" class="form-control" name="cep" id="cep" maxlength="9"
+                                           placeholder="Digite o CEP" required data-mask="00000-000">
                                 </div>
                                 <div class="form-group col-md-2">
                                     <label>&nbsp;</label><br>
@@ -77,66 +84,68 @@ $atracao = mysqli_query($con,$sql);
                             <div class="row">
                                 <div class="form-group col-md-6">
                                     <label for="rua">Rua: *</label>
-                                    <input type="text" class="form-control" name="rua" id="rua" placeholder="Digite a rua" maxlength="200" readonly>
+                                    <input type="text" class="form-control" name="rua" id="rua"
+                                           placeholder="Digite a rua" maxlength="200" readonly>
                                 </div>
                                 <div class="form-group col-md-3">
                                     <label for="numero">Número: *</label>
-                                    <input type="number" name="numero" class="form-control" placeholder="Ex.: 10" required>
+                                    <input type="number" name="numero" class="form-control" placeholder="Ex.: 10"
+                                           required>
                                 </div>
                                 <div class="form-group col-md-3">
                                     <label for="complemento">Complemento:</label>
-                                    <input type="text" name="complemento" class="form-control" maxlength="20" placeholder="Digite o complemento">
+                                    <input type="text" name="complemento" class="form-control" maxlength="20"
+                                           placeholder="Digite o complemento">
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="form-group col-md-4">
                                     <label for="bairro">Bairro: *</label>
-                                    <input type="text" class="form-control" name="bairro" id="bairro" placeholder="Digite o Bairro" maxlength="80" readonly>
+                                    <input type="text" class="form-control" name="bairro" id="bairro"
+                                           placeholder="Digite o Bairro" maxlength="80" readonly>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="cidade">Cidade: *</label>
-                                    <input type="text" class="form-control" name="cidade" id="cidade" placeholder="Digite a cidade" maxlength="50" readonly>
+                                    <input type="text" class="form-control" name="cidade" id="cidade"
+                                           placeholder="Digite a cidade" maxlength="50" readonly>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="estado">Estado: *</label>
-                                    <input type="text" class="form-control" name="estado" id="estado" maxlength="2" placeholder="Ex.: SP" readonly>
+                                    <input type="text" class="form-control" name="estado" id="estado" maxlength="2"
+                                           placeholder="Ex.: SP" readonly>
                                 </div>
                             </div>
                             <hr/>
-                            <?php
-                            foreach($atracao as $row)
-                            {
-                                if($row['valor_individual'] != 0.00){
-                            ?>
-                                    <div class="row">
-                                        <div class="form-group col-md-4">
-                                            <label for="banco">Banco: *</label>
-                                            <select id="banco" name="banco" class="form-control" required>
-                                                <option value="">Selecione um banco...</option>
-                                                <?php
-                                                geraOpcao("bancos", "");
-                                                ?>
-                                            </select>
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="agencia">Agência: *</label>
-                                            <input type="text" name="agencia" class="form-control"  placeholder="Digite a Agência" maxlength="12" required>
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="conta">Conta: *</label>
-                                            <input type="text" name="conta" class="form-control" placeholder="Digite a Conta" maxlength="12" required>
-                                        </div>
-                                    </div>
-                                    <hr/>
-                            <?php
-                                }
-                            }
-                            ?>
+
+                            <div class="row">
+                                <div class="form-group col-md-4">
+                                    <label for="banco">Banco: *</label>
+                                    <select id="banco" name="banco" class="form-control" required>
+                                        <option value="">Selecione um banco...</option>
+                                        <?php
+                                        geraOpcao("bancos", "");
+                                        ?>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="agencia">Agência: *</label>
+                                    <input type="text" name="agencia" class="form-control"
+                                           placeholder="Digite a Agência" maxlength="12" required>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="conta">Conta: *</label>
+                                    <input type="text" name="conta" class="form-control" placeholder="Digite a Conta"
+                                           maxlength="12" required>
+                                </div>
+                            </div>
+                            <hr/>
+
 
                             <div class="row">
                                 <div class="form-group col-md-12">
                                     <label for="observacao">Observação: </label>
-                                    <textarea id="observacao" name="observacao" rows="3" class="form-control"></textarea>
+                                    <textarea id="observacao" name="observacao" rows="3"
+                                              class="form-control"></textarea>
                                 </div>
                             </div>
 
