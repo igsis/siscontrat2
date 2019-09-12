302c4b169245b40b0981c7b0ddcecfc80412c4a5
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2019-09-12 13:08:34 -0300

    trocando a classificação indicativa

--- perfil/agendao/evento_edita.php
+++ perfil/agendao/evento_edita.php
@@ -347,53 +347,8 @@ include "includes/menu_interno.php";
     </div>
 </div>
 
+<?php @include "../perfil/includes/modal_classificacao.php"?>
 
-<!-- /modal -->
-<div class="modal fade" id="modal-default">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title"><strong>Classificação Indicativa</strong></h4>
-            </div>
-            <div class="modal-body">
-                <h4><strong>Informação e Liberdade de Escolha</strong></h4>
-                <p align="justify">A Classificação Indicativa é um conjunto de informações sobre o conteúdo de
-                    obras audiovisuais e diversões públicas quanto à adequação de horário, local e faixa etária.
-                    Ela alerta os pais ou responsáveis sobre a adequação da programação à idade de crianças e
-                    adolescentes. É da Secretaria Nacional de Justiça (SNJ), do Ministério da Justiça (MJ), a
-                    responsabilidade da Classificação Indicativa de programas TV, filmes, espetáculos, jogos
-                    eletrônicos e de interpretação (RPG).</p>
-                <p align="justify">Programas jornalísticos ou noticiosos, esportivos, propagandas eleitorais e
-                    publicidade, espetáculos circenses, teatrais e shows musicais não são classificados pelo
-                    Ministério da Justiça e podem ser exibidos em qualquer horário.</p>
-                <p align="justify">Os programas ao vivo poderão ser classificados se apresentarem inadequações,
-                    a partir de monitoramento ou denúncia.</p>
-                <p align="justify">
-                    <strong>Livre:</strong> Não expõe crianças a conteúdos potencialmente prejudiciais. Exibição
-                    em qualquer horário.<br>
-                    <strong>10 anos:</strong> Conteúdo violento ou linguagem inapropriada para crianças, ainda
-                    que em menor intensidade. Exibição em qualquer horário.<br>
-                    <strong>12 anos:</strong> As cenas podem conter agressão física, consumo de drogas e
-                    insinuação sexual. Exibição a partir das 20h.<br>
-                    <strong>14 anos:</strong> Conteúdos mais violentos e/ou de linguagem sexual mais acentuada.
-                    Exibição a partir das 21h.<br>
-                    <strong>16 anos:</strong> Conteúdos mais violentos ou com conteúdo sexual mais intenso, com
-                    cenas de tortura, suicídio, estupro ou nudez total. Exibição a partir das 22h.<br>
-                    <strong>18 anos:</strong> Conteúdos violentos e sexuais extremos. Cenas de sexo, incesto ou
-                    atos repetidos de tortura, mutilação ou abuso sexual. Exibição a partir das 23h.
-                </p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
-            </div>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-<!-- /.modal -->
 
 <script>
     let fomento = $('.fomento');
