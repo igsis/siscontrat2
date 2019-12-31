144914f9bcb3b3d9e0c6814b54610b859880431d
Lorelei <loreleigab@hotmail.com>
2019-12-31 14:49:59 -0300

    Botões de acesso no resultado da pesquisa

--- perfil/pagamento/resultado.php
+++ perfil/pagamento/resultado.php
@@ -1,6 +1,6 @@
 <?php
 $con = bancoMysqli();
-
+/* geral */
 if (isset($_POST['geral'])){
     $protocolo = $_POST['protocolo'] ?? NULL;
     $num_processo = $_POST['num_processo'] ?? NULL;
@@ -29,12 +29,14 @@ if (isset($_POST['geral'])){
     if ($operador_id != null && $operador_id != 0)
         $sqlOperador = "$ AND e.evento_operador_id = '$operador_id'";
 
-    $sql = "SELECT e.id, e.protocolo, p.numero_processo, p.pessoa_tipo_id, 
+    $sql = "SELECT e.id, p.id AS idPedido, e.protocolo, p.numero_processo, p.pessoa_tipo_id, 
     p.pessoa_fisica_id, p.pessoa_juridica_id, e.nome_evento, 
-    p.valor_total, ps.status, p.operador_id, p.data_kit_pagamento
+    p.valor_total, ps.status, u.nome_completo, p.data_kit_pagamento
     FROM eventos e 
     INNER JOIN pedidos p on e.id = p.origem_id 
     INNER JOIN pedido_status ps on p.status_pedido_id = ps.id
+    LEFT JOIN usuario_pagamentos up on p.operador_pagamento_id = up.usuario_id
+    LEFT JOIN usuarios u on up.usuario_id = u.id
     WHERE e.publicado = 1 
     AND p.publicado = 1 
     AND p.origem_tipo_id = 1
@@ -43,20 +45,15 @@ if (isset($_POST['geral'])){
     $sqlProjeto $sqlUsuario $sqlOperador
     $sqlProtocolo $sqlNomeEvento $sqlProcesso";
     $resultado = $con->query($sql);
+    $num_rows = mysqli_num_rows($resultado);
 }
 /* /.geral */
 ?>
 <div class="content-wrapper">
     <!-- Main content -->
     <section class="content">
-
         <!-- START FORM-->
         <h3 class="box-title">Resultado de busca</h3>
-        <div class="row" align="center">
-            <?php if (isset($mensagem)) {
-                echo $mensagem;
-            }; ?>
-        </div>
         <div class="row">
             <div class="col-md-12">
                 <div class="box">
@@ -77,33 +74,59 @@ if (isset($_POST['geral'])){
                                 <th>Status</th>
                                 <th>Operador</th>
                                 <th>Kit pagamento</th>
+                                <th>N.E.</th>
+                                <th>PGTO</th>
+                                <th>LIQ.</th>
                             </tr>
                             </thead>
                             <tbody>
                             <?php
-                            while ($evento = mysqli_fetch_array($resultado)) {
-                                if($evento['pessoa_tipo_id'] == 2){
-                                    $idPj = $evento['pessoa_juridica_id'];
-                                    $pj = $con->query("SELECT razao_social FROM pessoa_juridicas WHERE id = '$idPj'")->fetch_assoc();
-                                    $proponente = $pj['razao_social'];
-                                } else{
-                                    $idPf = $evento['pessoa_fisica_id'];
-                                    $pf = $con->query("SELECT nome FROM pessoa_fisicas WHERE id = '$idPf'")->fetch_assoc();
-                                    $proponente = $pf['nome'];
-                                }
+                            if ($num_rows == 0) {
                                 ?>
                                 <tr>
-                                    <td><?= $evento['numero_processo'] ?></td>
-                                    <td><?= $evento['protocolo'] ?></td>
-                                    <td><?= $proponente ?></td>
-                                    <td><?= $evento['nome_evento'] ?></td>
-                                    <td><?= retornaPeriodo($evento['id']) ?></td>
-                                    <td><?= dinheiroParaBr($evento['valor_total']) ?></td>
-                                    <td><?= $evento['status'] ?></td>
-                                    <td><?= "operador faltando" ?></td>
-                                    <td><?= $evento['data_kit_pagamento'] ? date('d/m/Y', strtotime($evento['data_kit_pagamento'])) : NULL ?></td>
+                                    <th colspan="12"><p align="center">Não foram encontrados registros</p></th>
                                 </tr>
-                            <?php
+                                <?php
+                            } else {
+                                while ($evento = mysqli_fetch_array($resultado)) {
+                                    if ($evento['pessoa_tipo_id'] == 2) {
+                                        $idPj = $evento['pessoa_juridica_id'];
+                                        $pj = $con->query("SELECT razao_social FROM pessoa_juridicas WHERE id = '$idPj'")->fetch_assoc();
+                                        $proponente = $pj['razao_social'];
+                                    } else {
+                                        $idPf = $evento['pessoa_fisica_id'];
+                                        $pf = $con->query("SELECT nome FROM pessoa_fisicas WHERE id = '$idPf'")->fetch_assoc();
+                                        $proponente = $pf['nome'];
+                                    }
+                                    ?>
+                                    <tr>
+                                        <td><?= $evento['numero_processo'] ?></td>
+                                        <td><?= $evento['protocolo'] ?></td>
+                                        <td><?= $proponente ?></td>
+                                        <td><?= $evento['nome_evento'] ?></td>
+                                        <td><?= retornaPeriodo($evento['id']) ?></td>
+                                        <td><?= dinheiroParaBr($evento['valor_total']) ?></td>
+                                        <td><?= $evento['status'] ?></td>
+                                        <td><?= $evento['nome_completo'] ? strstr($evento['nome_completo'],' ', true) : NULL ?></td>
+                                        <td><?= $evento['data_kit_pagamento'] ? date('d/m/Y', strtotime($evento['data_kit_pagamento'])) : NULL ?></td>
+                                        <td>
+                                            <form method="POST" action="?perfil=pagamento&p=empenho">
+                                                <button type="submit" class="btn btn-primary btn-block" name="idPedido" value="<?= $evento['idPedido'] ?>"><i class="fa fa-arrow-circle-right"></i></button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <form method="POST" action="?perfil=pagamento&p=pagamento">
+                                                <button type="submit" class="btn btn-primary btn-block" name="idPedido" value="<?= $evento['idPedido'] ?>"><i class="fa fa-arrow-circle-right"></i></button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <form method="POST" action="?perfil=pagamento&p=liquidacao">
+                                                <button type="submit" class="btn btn-primary btn-block" name="idPedido" value="<?= $evento['idPedido'] ?>"><i class="fa fa-arrow-circle-right"></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
                             }
                              ?>
                             </tbody>
@@ -118,6 +141,9 @@ if (isset($_POST['geral'])){
                                     <th>Status</th>
                                     <th>Operador</th>
                                     <th>Kit pagamento</th>
+                                    <th>N.E.</th>
+                                    <th>PGTO</th>
+                                    <th>LIQ.</th>
                                 </tr>
                             </tfoot>
                         </table>
