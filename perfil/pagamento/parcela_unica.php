a4b821459ebf5381dd75e88b7cfa6b3c21c2dc36
Lorelei <loreleigab@hotmail.com>
2020-01-06 12:52:50 -0300

    Relatório Declaração Simples

--- perfil/pagamento/parcela_unica.php
+++ perfil/pagamento/parcela_unica.php
@@ -238,7 +238,7 @@ $acesso = $con->query("SELECT * FROM usuario_pagamentos WHERE usuario_id = '$idU
                 $link14 = $server . "confirmacao_servico.php";
                 $link15 = $server . "minuta.php";
                 $link16 = $server . "emissao_nf_integral.php";
-                $link17 = $server . "documento.php";
+                $link17 = $server . "declaracao_simples.php";
                 $link18 = $server . "documento.php";
                 $link19 = $server . "documento.php";
                 $link20 = $server . "documento.php";
