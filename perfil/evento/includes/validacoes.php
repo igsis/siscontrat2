deea430e88e368a3ee7947894efd980bd79a443e
Lucas Dauto <lucasdauto79@gmail.com>
2020-01-13 22:58:32 -0300

    Validação de ocorrencia dentro do prazo

--- perfil/evento/includes/validacoes.php
+++ perfil/evento/includes/validacoes.php
@@ -190,19 +190,28 @@ if ($evento['tipo_evento_id'] == 1 && $pedidos != NULL) {
                         // VERIFICA SE ESTA DENTRO DO PRAZO
                         $hoje = new DateTime(date("Y-m-d"));
                         $dataInicio = new DateTime($ocorrencia['data_inicio']);
-                        $diff = $hoje->diff($dataInicio);
 
-                        if ($diff->days < 30) {
-                            $mensagem = "Hoje é dia " . $hoje->format('d/m/Y') . ". O seu evento se inicia em " . $dataInicio->format('d/m/Y') . ".<br>
+                        if($dataInicio<$hoje) {
+                            $diff = $hoje->diff($dataInicio);
+
+                            if ($diff->days < 30) {
+                                $mensagem = "Hoje é dia " . $hoje->format('d/m/Y') . ". O seu evento se inicia em " . $dataInicio->format('d/m/Y') . ".<br>
                                 O prazo para contratos é de 30 dias.<br>";
-                            $prazo = "Você está <b class='text-red'>fora</b> do prazo de contratos.";
-                            $fora = 1;
-                            break;
-                        } else {
+                                $prazo = "Você está <b class='text-red'>fora</b> do prazo de contratos.";
+                                $fora = 1;
+                                break;
+                            } else {
+                                $mensagem = "Hoje é dia " . $hoje->format('d/m/Y') . ". O seu evento se inicia em " . $dataInicio->format('d/m/Y') . ".<br>
+                                O prazo para contratos é de 30 dias.<br>";
+                                $prazo = "Você está <b class='text-green'>dentro</b> do prazo de contratos.";
+                                $fora = 0;
+                            }
+                        }else{
                             $mensagem = "Hoje é dia " . $hoje->format('d/m/Y') . ". O seu evento se inicia em " . $dataInicio->format('d/m/Y') . ".<br>
                                 O prazo para contratos é de 30 dias.<br>";
                             $prazo = "Você está <b class='text-green'>dentro</b> do prazo de contratos.";
                             $fora = 0;
+                        }
                         }
                     }
                 }
