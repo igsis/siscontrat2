79e3d7c74726180c688ef3839ad4d09d512688bd
BielMaggot <39780950+BielMaggot@users.noreply.github.com>
2020-04-15 16:46:00 -0300

    task #704

--- pdf/exporta_word_curadoria.php
+++ pdf/exporta_word_curadoria.php
@@ -5,7 +5,7 @@ require_once("../funcoes/funcoesGerais.php");
 
 $con = bancoMysqli();
 
-$sql = "SELECT id, nome_evento, sinopse FROM eventos WHERE publicado = 1 AND tipo_evento_id != 3";
+$sql = "SELECT id, tipo_evento_id, nome_evento, sinopse FROM eventos WHERE publicado = 1 AND tipo_evento_id != 3";
 
 $query = mysqli_query($con, $sql);
 //contador de eventos e de ocorrencias
@@ -20,34 +20,38 @@ while ($evento = mysqli_fetch_array($query)) {
     $e = $e + 1;
     echo "<p align='center'><b>Evento #</b>" . $e . "</p>";
     echo "<p align='justify'><b>Nome do Evento: </b>" . $evento['nome_evento'] . "</p>";
+    echo "<p align='justify'><b>Sinopse: </b>" . $evento['sinopse'] . "</p>";
     if ($evento['tipo_evento_id'] == 1) {
-        echo "<p align='justify'><b>Sinopse: </b>" . $atracao['sinopse'] . "</p>";
-        echo "<p align='justify'><b>Ficha Técnica: </b>" . $atracao['ficha_tecnica'] . "</p>";
-        echo "<p align='justify'><b>Classificação Indicativa: </b>" . $atracao['classificacao_indicativa'] . "</p>";
-        echo "<p>&nbsp;</p>";
-
-        $cronograma = $con->query("SELECT * FROM ocorrencias WHERE origem_ocorrencia_id = " . $evento['id'] . " AND tipo_ocorrencia_id = 1 AND publicado = 1");
-        $o = $o + 1;
-        while ($aux = mysqli_fetch_array($cronograma)) {
-            echo "<p align='center'><b>Ocorrencia #" . $o . " do Evento: " . $evento['nome_evento'] . "</b></p>";
-            $checaTipo = $con->query("SELECT acao_id FROM acao_atracao WHERE atracao_id = " . $evento['idAtracao'])->fetch_array();
-            $tipoAcao = $con->query("SELECT acao FROM acoes WHERE id = " . $checaTipo['acao_id'] . " AND publicado = 1")->fetch_array();
-            $acao = $tipoAcao['acao'];
-
-            $dia = retornaPeriodoNovo($aux['origem_ocorrencia_id'], 'ocorrencias');
-            $hour = exibirHora($aux['horario_inicio']) . "h - " . exibirHora($aux['horario_fim']) . "h";
-            $local = $con->query("SELECT local FROM locais WHERE id = " . $aux['local_id'] . " AND publicado = 1")->fetch_array();
-            $lugar = $local['local'];
-
-            echo "<p align='justify'><b>Ação: </b>" . $acao . "</p>";
-            echo "<p align='justify'><b>Data/Perído: </b>" . $dia . "</p>";
-            echo "<p align='justify'><b>Horário: </b>" . $hour . "</p>";
-            echo "<p align='justify'><b>Local: </b>" . $lugar . "</p>";
-
+        $atracoes = $con->query("SELECT a.id, a.ficha_tecnica, ci.classificacao_indicativa FROM atracoes a INNER JOIN classificacao_indicativas ci on a.classificacao_indicativa_id = ci.id WHERE publicado = 1 AND evento_id = " . $evento['id']);
+        foreach ($atracoes as $atracao) {
+            echo "<p align='justify'><b>Ficha Técnica: </b>" . $atracao['ficha_tecnica'] . "</p>";
+            echo "<p align='justify'><b>Classificação Indicativa: </b>" . $atracao['classificacao_indicativa'] . "</p>";
             echo "<p>&nbsp;</p>";
-            echo "<p>&nbsp;</p>";
+
+            $cronograma = $con->query("SELECT * FROM ocorrencias WHERE origem_ocorrencia_id = " . $evento['id'] . " AND tipo_ocorrencia_id = 1 AND publicado = 1");
+            $o = $o + 1;
+            while ($aux = mysqli_fetch_array($cronograma)) {
+                echo "<p align='center'><b>Ocorrencia #" . $o . " do Evento: " . $evento['nome_evento'] . "</b></p>";
+                $checaTipo = $con->query("SELECT acao_id FROM acao_atracao WHERE atracao_id = " . $atracao['id'])->fetch_array();
+                $tipoAcao = $con->query("SELECT acao FROM acoes WHERE id = " . $checaTipo['acao_id'] . " AND publicado = 1")->fetch_array();
+                $acao = $tipoAcao['acao'];
+
+                $dia = retornaPeriodoNovo($aux['origem_ocorrencia_id'], 'ocorrencias');
+                $hour = exibirHora($aux['horario_inicio']) . "h - " . exibirHora($aux['horario_fim']) . "h";
+                $local = $con->query("SELECT local FROM locais WHERE id = " . $aux['local_id'] . " AND publicado = 1")->fetch_array();
+                $lugar = $local['local'];
+
+                echo "<p align='justify'><b>Ação: </b>" . $acao . "</p>";
+                echo "<p align='justify'><b>Data/Perído: </b>" . $dia . "</p>";
+                echo "<p align='justify'><b>Horário: </b>" . $hour . "</p>";
+                echo "<p align='justify'><b>Local: </b>" . $lugar . "</p>";
+
+                echo "<p>&nbsp;</p>";
+                echo "<p>&nbsp;</p>";
+
+            }
+            $o = 0;
         }
-        $o = 0;
 
     } elseif ($evento['tipo_evento_id'] == 2) {
         $filmes = $con->query("SELECT id, filme_id FROM filme_eventos WHERE evento_id = " . $evento['id']);
